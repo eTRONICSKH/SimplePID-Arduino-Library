9ee70ee109ef183559d1165784cb7561230dd36b
Ktheara <thera729@gmail.com>
2020-06-14 14:53:32 +0700

    added config func

--- simplePID.cpp
+++ simplePID.cpp
@@ -1,17 +1,75 @@
 //Need to update by dev 2
 #include "simplePID.h"
 
-simplePID::simplePID(float kp, float ki): _kp(kp), _ki(ki), _limit(1.0){
+simplePID::simplePID(bool pid_mode): _pid_mode(pid_mode){
   _intergral = 0.0;
+  _last_timer = (unsigned long)millis();
+}
+
+void simplePID::SetSampleTime(unsigned long new_sample_time){
+  _sample_time_ms = new_sample_time;
+}
+
+void simplePID::SetTunings(unsigned double kp, unsigned double ki, unsigned double kd){
+  _kp = kp;
+  _ki = ki;
+  _kd = kd;
+}
+
+void simplePID::SetOutputRange(double output_min, double output_max){
+  _output_min = output_min;
+  _output_max = output_max;
+}
+
+void simplePID::SetMotorParameters(double gear_ratio, double pule_per_revolution, double revolution_unit){
+  _motor_gear = gear_ratio;
+  _motor_ppr = pule_per_revolution;
+  _motor_revolution_unit = revolution_unit;
+}
+
+double simplePID::MotorComputeLoop(double setpoint, long encoder_input){
+  if (_pid_mode != PID_MOTOR_CONTROL) return;
+
+  _elapsed_time = (unsigned long)millis()-_last_timer;
+  if (_elapsed_time >= _sample_time_ms){
+    _motor_encoder = encoder_input;
+    _elapsed_revolution = (double)(_motor_last_encoder-_motor_encoder)/(_motor_gear*_motor_ppr);
+    _motor_revolution += _elapsed_revolution;
+    _motor_speed  = _motor_revolution_unit *  _elapsed_revolution / _elapsed_time;
+    _motor_last_encoder = _motor_encoder;
+    _last_timer = (unsigned long)millis();
+    _calc_input = _motor_speed;
+  }
+
+  return(simplePID::cmd(setpoint, _calc_input, _elapsed_time));
+}
+
+double simplePID::MotorSpeed(){
+  return _motor_speed;
+}
+
+double simplePID::MotorRevolution(){
+  return _motor_revolution;
+}
+
+double simplePID::InputComputeLoop(double setpoint, double raw_input){
+  if (_pid_mode != PID_INPUT_CONTROL) return;
+  _elapsed_time = (unsigned long)millis()-_last_timer;
+  if (_elapsed_time >= _sample_time_ms) {
+    _calc_input = raw_input;
+  }
+
+  return(simplePID::cmd(setpoint, _calc_input, _elapsed_time));
 }
 
 
-float simplePID::cmd(float desire, float current, long dt){
-  _error = desire - current;
-  _intergral += _error * dt/1000.0;
-  _intergral = max(-_limit, min(_limit, _intergral));
-  _cmd = _kp*_error + _ki*_intergral;
-  return round(max(-255, min(255, _cmd)));
+double simplePID::cmd(double setpoint, double calc_input, long elapsed_time){
+  _error = setpoint - calc_input;
+  _intergral += _error * (double)elapsed_time;
+  _derivative = (_error - _last_error)/(double)elapsed_time;
+  _cmd = _kp*_error + _ki*_intergral + _kd*_derivative;
+  _last_error = _error;
+  return round(constrain(_cmd, _output_min, _output_max));
 }
 
 
